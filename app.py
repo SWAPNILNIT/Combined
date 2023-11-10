4aefe670be5d2c73ad2905003227f3a18893690c
SWAPNILNIT <147801634+SWAPNILNIT@users.noreply.github.com>
2023-11-10 23:00:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,11 @@ model_finance.eval()
 
 print("Peft health & finance model loaded")
 
+openai.api_type = type
+openai.api_base = base
+openai.api_version = version
+os.environ["OPENAI_API_KEY"] = key
+openai.api_key = key
 
 def remove_repeated_phrases_and_sentences(text):
   # Split the text into sentences
